cc415c06f31596f80f9f13c4a04ebf06c8714b7d
dhslrl321 <dhslrl321@gmail.com>
2020-12-07 16:22:26 +0900

    [deploy] execute shell script update

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -5,7 +5,7 @@ cp -f /nginx.conf /etc/nginx
 cd ./docker
 cp -f ./docker-compose.blue.yml /home/ec2-user/docker-image
 cp -f ./docker-compose.green.yml /home/ec2-user/docker-image
-cp -f ./Dockerfile.yml /home/ec2-user/docker-image
+cp -f ./Dockerfile /home/ec2-user/docker-image
 cd -f ./../script
 cp -f ./deploy.sh /home/ec2-user/docker-image
 cp -f ./start-server.sh /home/ec2-user/docker-image
