0fa2a0e10abacbefcaf434dae661eff0b661d5af
dhslrl321 <dhslrl321@gmail.com>
2020-12-07 16:30:20 +0900

    [deploy] execute shell script update

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -11,4 +11,5 @@ cp -f ./deploy.sh /home/ec2-user/docker-image
 cp -f ./start-server.sh /home/ec2-user/docker-image
 
 cd /home/ec2-user/docker-image
+sudo chmod +x ./deploy.sh
 ./deploy.sh > /dev/null 2> /dev/null < /dev/null &
