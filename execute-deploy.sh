f2ef2ea244f5a5df91278f8aa934f939549d595b
dhslrl321 <dhslrl321@gmail.com>
2020-12-07 16:25:05 +0900

    [deploy] execute shell script update

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -6,7 +6,7 @@ cd ./docker
 cp -f ./docker-compose.blue.yml /home/ec2-user/docker-image
 cp -f ./docker-compose.green.yml /home/ec2-user/docker-image
 cp -f ./Dockerfile /home/ec2-user/docker-image
-cd -f ./../script
+cd /home/ec2-user/deploy/Bless-Music-Studio/script
 cp -f ./deploy.sh /home/ec2-user/docker-image
 cp -f ./start-server.sh /home/ec2-user/docker-image
 
