3c3b7884b10a9f7f8c37260515108c176f93da69
dhslrl321 <dhslrl321@gmail.com>
2020-12-07 11:50:20 +0900

    [deploy] deploy.sh 수정

--- script/deploy.sh
+++ script/deploy.sh
@@ -5,14 +5,16 @@ DOCKER_APP_NAME=bless-music-studio-server
 EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
 
 if [ -z "$EXIST_BLUE" ]; then
-	echo "blue up"
+	echo "blue up now : "
+	echo EXIST_BLUE
 	docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d
 
 	sleep 10
 
 	docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
 else
-	echo "green up"
+	echo "green up now : "
+	echo EXIST_BLUE
 	docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d
 
 	sleep 10
