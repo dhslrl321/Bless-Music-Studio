bf81ab6571dd67e25475a89889f9918d76bcfbfe
dhslrl321 <dhslrl321@gmail.com>
2020-12-07 16:15:51 +0900

    [deploy] execute shell script update

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -2,11 +2,12 @@
 
 cd /home/ec2-user/deploy/Bless-Music-Studio
 cp -f /nginx.conf /etc/nginx
-cp -f /docker-compose/docker-compose.blue.yml /home/ec2-user/docker-image
-cp -f /docker-compose/docker-compose.green.yml /home/ec2-user/docker-image
-cd -f /script
-cp -f deploy.sh /home/ec2-user/docker-image
-cp -f start-server.sh /home/ec2-user/docker-image
+cd ./docker
+cp -f ./docker-compose.blue.yml /home/ec2-user/docker-image
+cp -f ./docker-compose.green.yml /home/ec2-user/docker-image
+cd -f ../script
+cp -f ./deploy.sh /home/ec2-user/docker-image
+cp -f ./start-server.sh /home/ec2-user/docker-image
 
 cd /home/ec2-user/docker-image
 ./deploy.sh > /dev/null 2> /dev/null < /dev/null &
