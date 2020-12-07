7cbe2ad5c13468af5a7f8c45cfec1e19690e4be9
dhslrl321 <dhslrl321@gmail.com>
2020-12-07 19:29:00 +0900

    [client] test span added

--- client/src/sections/HeroSection/index.jsx
+++ client/src/sections/HeroSection/index.jsx
@@ -18,7 +18,7 @@ const HeroSection = () => {
       <HeroContent>
         <HeroTitle>Bless Music Studio</HeroTitle>
         <HeroDescription>
-          Bless Music Studio에서 당신의 꿈을 키워보세요.
+          Bless Music Studio에서 당신의 꿈을 키워보세요. (up)
         </HeroDescription>
       </HeroContent>
     </HeroContainer>
